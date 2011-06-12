8ef244c350b14880d0c88df6758fc9eb84c0f1d7
Charles Thompson <charleswthompsoniii@gmail.com>
2011-06-11 20:44:44 -0700

    refactoring strutil

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -1,20 +1,26 @@
 package strutil
 
 import "os"
-import "fmt"
 
-func SearchUsingBruteForce(pattern string, text string) (os.Error, int) {
+func SearchUsingBruteForce(pattern string, text string) (int, os.Error) {
     if text == "" || pattern == "" {
-        return os.NewError("text cannot be empty"), 0
+        return 0, os.NewError("text cannot be empty")
     }
 
-    runes_pattern = []int(pattern)
-    runes_text = []int(text)
+    runes_pattern := []int(pattern)
+    runes_text := []int(text)
 
     i := 0
 
     for i <= len(runes_text) - len(runes_pattern) {
-         
+        x := 0    
+        for x < len(runes_pattern) {
+            if runes_text[i + x] == runes_pattern[x]
+        }
+
     }
+
+    return 0, os.NewError("pattern not found")
     
 }
+
--- strutil/strutil_test.go
+++ strutil/strutil_test.go
@@ -1,8 +1,18 @@
 package strutil
 
 import "testing"
+import "fmt"
 
 func TestFind(t *testing.T) {
-    //t.Errorf("Not implemented.")
-    return
+    str := "String Matching"
+    pattern := "Str"
+
+    pos, err := SearchUsingBruteForce(pattern, str)
+
+    fmt.Printf("pos = %d\n", pos)
+
+    if pos != 1 || err == nil {
+        t.Errorf("pos should be 0 and err should be nil\n")
+    }
+
 }
